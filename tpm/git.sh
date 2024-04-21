c12637df30e6c4594dd3684267400d7928b60f45
Brian Duranleau <b.duranleau@outlook.com>
2024-04-21 10:52:58 -0500

    fix: make git icon more readable

--- tpm/git.sh
+++ tpm/git.sh
@@ -1,14 +1,14 @@
 # Requires https://github.com/bduranleau/tmux-git
 
 show_git() {
-    local index icon color text module
+	local index icon color text module
 
-    index=$1
-    icon="$(get_tmux_option "@catppuccin_git_icon" "")"
-    color="$(get_tmux_option "@catppuccin_git_color" "$thm_red")"
-    text="$(get_tmux_option "@catppuccin_git_text" "#{git_branch}")"
+	index=$1
+	icon="$(get_tmux_option "@catppuccin_git_icon" "")"
+	color="$(get_tmux_option "@catppuccin_git_color" "$thm_red")"
+	text="$(get_tmux_option "@catppuccin_git_text" "#{git_branch}")"
 
-    module=$(build_status_module "$index" "$icon" "$color" "$text")
+	module=$(build_status_module "$index" "$icon" "$color" "$text")
 
-    echo "$module"
+	echo "$module"
 }
